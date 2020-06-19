370c408101e82722dceb874b5e881168a8d50e52
BGLCROP/sqiu <sqiu@bglcorp.com.au>
2020-06-19 14:36:25 +1000

    Review2 commit

--- website/app.js
+++ website/app.js
@@ -4,20 +4,22 @@ const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
 const apiKey = "&appid=a1e6c35d46106a09dfb90e59ac8943aa&units=imperial";
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
 
 const performAction = () => {
   const zip = document.getElementById("zip").value;
   const userResponse = document.getElementById("feelings").value;
-  getWeatherData(baseURL, zip, apiKey).then((data) => {
-    postData("/add", {
-      date: newDate,
-      temp: data.main.temp,
-      userResponse,
-    }).then(() => {
-      updateUI();
+  if (zip !== null || userResponse !== null) {
+    getWeatherData(baseURL, zip, apiKey).then((data) => {
+      postData("/add", {
+        date: newDate,
+        temp: data.main.temp,
+        userResponse,
+      }).then(() => {
+        updateUI();
+      });
     });
-  });
+  }
 };
 const getWeatherData = async (baseURL, zip, apiKey) => {
   const res = await fetch(baseURL + zip + apiKey);
@@ -53,6 +55,7 @@ const updateUI = async () => {
     document.getElementById("content").innerHTML = allData.userResponse;
     console.log("updateUI completed");
   } catch (error) {
+    document.getElementById("content").innerHTML = "error!";
     console.log("error", error);
   }
 };
