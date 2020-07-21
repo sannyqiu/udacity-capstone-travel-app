2bb1880693a9de19e8268f6c2487e3976aba147d
BGLCROP/sqiu <sqiu@bglcorp.com.au>
2020-07-22 00:37:44 +1000

    Adjusted css

--- src/client/index.js
+++ src/client/index.js
@@ -11,8 +11,6 @@ document.getElementById("generate").addEventListener("click", performAction);
 
 import "./styles/style.scss";
 import "./styles/header.scss";
-import "./styles/footer.scss";
-import "./styles/input.scss";
 import "./styles/output.scss";
 
 export {
